2e2560d93eef9eff9b7a6cc07710d0f52ee82d2f
bhighlander <saphrimangel@gmail.com>
2023-11-20 15:41:40 -0600

    Add Outfit and OutfitArticle models and views

--- cassisapi/models/__init__.py
+++ cassisapi/models/__init__.py
@@ -2,3 +2,5 @@ from .fashionista import Fashionista
 from .article import Article
 from .color import Color
 from .type import Type
+from .outfit import Outfit
+from .outfit_article import OutfitArticle
---
+++ cassisapi/models/outfit_article.py
@@ -0,0 +1,7 @@
+"""Many to many relationship between Outfit and Article"""
+
+from django.db import models
+
+class OutfitArticle(models.Model):
+    outfit = models.ForeignKey("Outfit", on_delete=models.CASCADE)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
